f919c2a78f9155db6f0ef0c813532e42594b8c0a
loshan20011 <loshan20011@gmail.com>
2024-02-03 00:55:55 +0530

    schema-created

--- model/index.js
+++ model/index.js
@@ -1,6 +1,31 @@
 const mongoose = require('mongoose');
 const dataSchema = new mongoose.Schema(
     {
-        
+        title: {
+            required: true,
+            type: String,
+        },
+        year: {
+            required: true,
+            type: String,
+        },
+        runTime: {
+            required: true,
+            type: String,
+        },
+        genre: {
+            required: true,
+            type: String,
+        },
+        director: {
+            required: true,
+            type: String,
+        },
+        image: {
+            required: true,
+            type: Array,
+        }
     }
 )
+
+module.exports = mongoose.model('Data', dataSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const model = require('../model');
 
 const router = express.Router();
 
-router.post('/movie', (req,res) => {
-    res.send('Post API')
+router.post('/movie', async (req,res) => {
+    const data = new model(
+        {
+            title: req.body.title,
+            year: req.body.year,
+            runTime: req.body.runTime,
+            genre: req.body.genre,
+            director: req.body.director,
+            image: req.body.image
+        }
+    )
+
+    try{
+        const dataToSave = await data.save();
+        res.status(201).json(dataToSave);
+    }catch(err){
+        res.status(400).json(
+            {
+                message: err.message
+            }
+            )}
 })
 
 module.exports = router;
